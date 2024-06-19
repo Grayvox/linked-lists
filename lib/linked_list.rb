c47cbd830990a6621886a5dd65eda395da9b0bfd
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-19 11:07:40 -0400

    Add LinkedList class
    Adds the LinkedList class along with a default
    value for 'head' set to nil

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+require_relative 'node'
+
+# Defines the linked list itself
+class LinkedList
+  attr_accessor :head
+
+  def intialize(head = nil)
+    @head = head
+  end
+end
