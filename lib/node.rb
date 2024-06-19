f17d4bfe885d6e0147b95e4393ff63ff0f8b837f
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-19 11:01:02 -0400

    Add Node class
    Adds variables for the Node's value and next_node

--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+# Defines a node in a linked list
+class Node
+  attr_reader :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
